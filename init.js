6ef1f6db98cad09a8cddd81f028afe30a2e010b5
ietzel <67884277+ietzel@users.noreply.github.com>
2023-01-31 19:14:21 -0800

    Add files via upload

--- init.js
+++ init.js
@@ -1,15 +1,75 @@
-document.getElementById("create Character").addEventListener("click", function() {
-    characters.push(new createCharacter(document.getElementById("character").value));
+var running = false;
+const scales = ["(local)", "(geographic)", "(planar)"];
+var scale = 1;
+
+document.getElementById("play").addEventListener("click", function() {
+	if(running) {
+		running = false;
+		document.getElementById("play").innerHTML = "play";
+	} else {
+		running = true;
+		document.getElementById("play").innerHTML = "pause";
+		characters[0][0] = new createCharacter(
+			document.getElementById("character_name").value, 
+			parseFloat(document.getElementById("ethics").value).toFixed(1), 
+			parseFloat(document.getElementById("morality").value).toFixed(1), 
+			document.getElementById("str").value, 
+			document.getElementById("dex").value, 
+			document.getElementById("con").value, 
+			document.getElementById("int").value, 
+			document.getElementById("wis").value, 
+			document.getElementById("cha").value, 
+			document.getElementById("celestial").checked || document.getElementById("fiendish").checked,
+			document.getElementById("h_d").checked,
+			document.getElementById("s_c").checked,
+			document.getElementById("lycanthrope").checked,
+			document.getElementById("giant").checked,
+			document.getElementById("advanced").checked
+		);
+		/*
+		for(var i=0; i<Skills.length; i++) {
+			console.log(document.getElementById(Skills[i].name));
+			if(document.getElementById(Skills[i].name).checked) {
+				characters[0].class_skills.push(Skills[i]);
+			}
+		}
+		*/
+	}	
 });
 
-document.getElementById("negotiate/profiteer").addEventListener("click", function() {
-	negotiate_profiteer();
-});
+function cb_limit() {
+	let total = 0, limit = 10;
+	for(var i=0; i < document.nC.skill.length; i++){
+		if(document.nC.skill[i].checked){
+			total++;
+		}
+		if(total > limit){
+			alert("10 class skills must be selected") 
+			document.nC.skill[i].checked = false ;
+			return false;
+		}
+	}
+}
+
+function randomInt(a, b) {
+	return (Math.random()*(b-a))+a;
+}
+
+function roll(n, s) {
+	let total = 0;
+	for(var i=0; i<n; i++) {
+		total += randomInt(1, s);
+	}
+	return total;
+}
 
 var canvas = new fabric.Canvas("canvas");
 
-const radius = 7; 
-const slant = 6;
+const radius = 7, slant = 6;
+const width = 24, height = 21;
+let clickX = canvas.width, clickY = canvas.height;
+let partyX = Math.round(width/2, 0), partyY = Math.round(height/2, 0);
+let characterX = partyX, characterY = partyY, groupX = partyX, groupY = partyY;
 
 let noiseScale = 1/150
 let ocean = "#008dc4";
@@ -19,82 +79,276 @@ let grass = "#7ec850";
 let stone = "#676767";
 let snow = "#fffafa";
 
-class Character {
-	constructor(x, y, r, name) {
-		this.x = x;
-		this.y = y;
-		this.r = r;
+function createCharacter(name, ethics, morals, str, dex, con, int, wis, cha, c_or_f, h_d, s_c, lycanthrope, giant, advanced, shapechange) {
+	this.name = "Character";
+	this.ethics = 0.5;
+	this.morals = 0.5;
+	this.level = 1;
+	this.XP = 200;
+	this.budget = Math.round(787*Math.pow(2, 0.365*this.level), 2);
+	this.racial_HD = 0;
+	this.size = "M";
+	this.type = "Humanoid";
+	this.shapechange;
+	this.abilities = [10, 10, 10, 11, 11, 11]; //str, dex, con, int, wis, cha
+	this.templates = [false, false, false, false, false, false]; //c_or_f, h_d, s_c, lycanthrope, giant, advanced
+	this.speeds = [30, 0, 0, 0, 0, 0]; //land, climb, fly, swim, burrow;
+	this.x = 0;
+	this.y = 0;
+	this.bonuses = [0, 0]; //nat armor, initiative
+	this.class_skill_limit = 10;
+	this.class_skills = [];
+	if(name) {
 		this.name = name;
 	}
-}
-
-function createCharacter(name) {
-    this.name = "";
-    //this. = [];
-    if(name) {
-        this.name = name;
-    }
-}
-
-class Hexagon {
-	constructor(x, y, r, name) {
-		this.x = x;
-		this.y = y;
-		this.r = r;
-		this.hex;
-		this.name = name;
+	if(ethics) {
+		this.ethics = 0.5;
 	}
-	/*
-	function distanceToMouse() {
-		return dist(pointer.x, pointer.y, this.x, this.y);
+	if(morals) {
+		this.morals = 0.5;
 	}
-	*/
+	if(str) {
+		this.abilities[0] = str;
+	}
+	if(dex) {
+		this.abilities[0] = dex;
+	}
+	if(con) {
+		this.abilities[2] = con;
+	}
+	if(int) {
+		this.abilities[3] = int;
+	}
+	if(wis) {
+		this.abilities[4] = wis;
+	}
+	if(cha) {
+		this.abilities[5] = cha;
+	}
+	if(c_or_f) {
+		this.templates[0] = true;
+	}
+	if(h_d) {
+		this.templates[1] = true;
+		this.abilities[0] += 8;
+		this.abilities[2] += 6;
+		this.abilities[3] += 2;
+		this.abilities[5] += 2;
+		this.bonuses += 4;
+		this.speeds[2] = this.speeds[0]*2; 
+	}
+	if(s_c) {
+		this.templates[2] = true;
+		this.class_skills.push(Skills[0]);
+		this.class_skills.push(Skills[2]);
+		this.class_skills.push(Skills[7]);
+		this.class_skills.push(Skills[25]);
+		this.class_skills.push(Skills[32]);
+		this.racial_HD = 2;
+		this.type = "Undead";
+		this.abilities[0] += 2;
+		this.abilities[1] += 2;
+		this.bonuses[0] += 2;
+	}
+	if(lycanthrope) {
+		this.shapechange = shapechange;
+		this.templates[3] = true;
+		this.abilities[0] += 2+((this.shapechange+1)*2);
+		this.abilities[1] -= this.shapechange*2;
+		this.abilities[2] += 2;
+		this.abilities[4] += 2;
+		this.abilities[5] -= 2;
+		this.bonuses[0] += 2+this.shapechange;
+	}
+	if(giant) {
+		this.templates[4] = true;
+		this.size = "L";
+		this.abilities[0] += 8;
+		this.abilities[1] -= 2;
+		this.abilities[2] += 4;
+		this.bonuses[0] += 3;
+	}
+	if(advanced) {
+		this.templates[5] = true;
+		this.abilities[0] += 4;
+		this.abilities[1] += 4;
+		this.abilities[2] += 4;
+		this.abilities[3] += 4;
+		this.abilities[4] += 4;
+		this.abilities[5] += 4;
+		this.bonuses[0] += 2;
+	}
+	this.f_updateSaves = function([fort, ref, will]) {
+		this.saves = [fort, ref, will];
+	}
+	this.f_updateMvmt = function(run_mult, init_bonus) {
+		this.run_mult = run_mult; 
+		this.init_bonus = init_bonus; 
+	}
+	this.f_updatePoints = function(skill, hp_bonus) {
+		this.skill = skill; 
+		this.hp_bonus = hp_bonus; 
+	}	
 }
 
-function pickColor(i, j) {
-  let h = ((perlin.get((i)*noiseScale, (j)*noiseScale))+1)/2;
-  let c = "#facade";
+const Feats = [
+	"Great Fortitude",
+	"Iron Will",
+	"Lightning Reflexes",
+	"Run",
+	"Imp. Initiative",
+	"Skill Focus",
+	"Toughness"
+]
+
+const Skills = [
+	new createSkill("Climb", "str"),
+	new createSkill("Swim", "str"),
+	new createSkill("Fly", "dex"),
+	new createSkill("Acrobatics", "dex"),
+	new createSkill("Disable Device", "dex"),
+	new createSkill("Escape Artist", "dex"),
+	new createSkill("Sleight of Hand", "dex"),
+	new createSkill("Stealth", "dex"),
+	new createSkill("Craft(alchemy)", "int"),
+	new createSkill("Craft(cloth/clothing)", "int"),
+	new createSkill("Craft(food)", "int"),
+	new createSkill("Craft(traps)", "int"),
+	new createSkill("Craft(arms&armor)", "int"),
+	new createSkill("Knowledge(engineering)", "int"),
+	new createSkill("Knowledge(history)", "int"),
+	new createSkill("Knowledge(nature)", "int"),
+	new createSkill("Knowledge(planes)", "int"),
+	new createSkill("Profession(woodcutter)", "wis"),
+	new createSkill("Profession(fisherman)", "wis"),
+	new createSkill("Profession(farmer)", "wis"),
+	new createSkill("Profession(miner)", "wis"),
+	new createSkill("Profession(soldier)", "wis"),
+	new createSkill("Profession(cook)", "wis"),
+	new createSkill("Profession(architect/engineer)", "wis"),
+	new createSkill("Heal", "wis"),
+	new createSkill("Perception", "wis"),
+	new createSkill("Perform(percussion/keyboards)", "cha"),
+	new createSkill("Perform(strings)", "cha"),
+	new createSkill("Perform(winds) ", "cha"),
+	new createSkill("Perform(act/dance)", "cha"),
+	new createSkill("Perform(sing/oratory)", "cha"),
+	new createSkill("Diplomacy", "cha"),
+	new createSkill("Disguise", "cha")
+];
+
+function createType(name, HD_type, skills_p_l, BAB, [fort, ref, will], [llv, dv, scent], []) {
+	this.name = name;
+	this.HD_type = 8;
+	this.skills_p_l = skills_p_l; 
+	this.BAB = BAB;
+	this.saves = [fort, ref, will]; 
+	this.senses = [llv, dv, scent];
+	this.class_skills = [];
+}	
+
+function createAttack(name, atk_mod, dmg_mod, ...dmg) {
+	this.name = name;
+	this.atk_mod = atk_mod;
+	this.dmg_mod = dmg_mod;
+	this.dmg = [dmg];
+}	
+
+function createDamage(name, dmg, type) {
+	this.name = name;
+	this.dmg = dmg;
+	this.type = type;
+}	
+
+function createSkill(name, mod) {
+	this.name = name;
+	this.mod = mod;
+	this.ranks = 0;
+	this.class_skill = false;
+}
+
+function createEquipment(name, mod, target, value, weight, [attack]) {
+	this.name = name;
+	this.mod = mod;
+	this.target = target;
+	this.value = value, 
+	this.weight = weight;
+	this.abilities = [attack];	
+}	
+
+function pickColor(i, j, h) {
+	let c = "#facade";
   
-  if(h < 0.2) {
-    c = ocean;
-  } else if(h < 0.3) {
-    if(Math.random() > Math.pow(h-0.2, 2)*100) {
-      c = ocean;
-    } else {
-      c = shore;
-    }
-  } else if(h < 0.4) {
-    if(Math.random() > Math.pow(h-0.3, 2)*100) {
-      c = shore;
-    } else {
-      c = sand;
-    }
-  } else if(h < 0.5) {
-    if(Math.random() > Math.pow(h-0.4, 2)*100) {
-      c = sand;
-    } else {
-      c = grass;
-    }
-  } else if(h < 0.6) {
-    if(Math.random() > Math.pow(h-0.5, 2)*100) {
-      c = grass;
-    } else {
-      c = stone;
-    }
-  } else if (h < 0.7) {
-    if(Math.random() > Math.pow(h-0.6, 2)*100) {
-      c = stone;
-    } else {
-      c = snow;
-    }
-  } else {
-    c = snow;
-  }
-  return c;
+	if(h < 0.2) {
+		c = ocean;
+	} else if(h < 0.3) {
+		if(Math.random() > Math.pow(h-0.2, 2)*100) {
+			c = ocean;
+		} else {
+			c = shore;
+		}
+	} else if(h < 0.4) {
+		if(Math.random() > Math.pow(h-0.3, 2)*100) {
+			c = shore;
+		} else {
+			c = sand;
+		}
+	} else if(h < 0.5) {
+		if(Math.random() > Math.pow(h-0.4, 2)*100) {
+			c = sand;
+		} else {
+			c = grass;
+		}
+	} else if(h < 0.6) {
+		if(Math.random() > Math.pow(h-0.5, 2)*100) {
+			c = grass;
+		} else {
+			c = stone;
+		}
+	} else if (h < 0.7) {
+		if(Math.random() > Math.pow(h-0.6, 2)*100) {
+			c = stone;
+		} else {
+			c = snow;
+		}
+	} else {
+		c = snow;
+	}
+    return c;	
 }
 
-function createHexagon(x_pos, y_pos) {
-	var hexagon = new fabric.Polygon(
+function createSquare(x, y, x_pos, y_pos, c) {
+	this.x = x;
+	this.y = y;
+	this.x_pos = x_pos;
+	this.y_pos = y_pos;
+	this.tetra = new fabric.Polygon(
+		[
+			{ x: x_pos, y: y_pos },
+			{ x: x_pos+radius, y: y_pos },
+			{ x: x_pos+radius, y: y_pos+radius },
+			{ x: x_pos, y: y_pos+radius },
+		], 
+		{
+			fill: c,  
+            stroke: "black",
+			strokeWidth: 0.33,
+			//borderColor: 'black',
+			//cornerColor: 'black',
+			selectable: false
+		}
+	);
+}
+
+function createHexagon(x, y, x_pos, y_pos) {
+	this.x = x;
+	this.y = y;
+	this.x_pos = x_pos;
+	this.y_pos = y_pos;
+	this.elevation = ((perlin.get((x_pos)*noiseScale, (y_pos)*noiseScale))+1)/2;
+	this.c = pickColor(x, y, this.elevation);
+	this.grid = [];
+	this.hex = new fabric.Polygon(
 		[
 			{ x: x_pos, y: y_pos },
 			{ x: x_pos+radius, y: y_pos },
@@ -104,7 +358,7 @@ function createHexagon(x_pos, y_pos) {
 			{ x: x_pos-(radius*0.5), y: y_pos+slant }
 		], 
 		{
-			fill: pickColor(x_pos, y_pos),
+			fill: this.c,
 			stroke: 'black',  
             strokeWidth: 0.5,
 			//borderColor: 'black',
@@ -112,5 +366,35 @@ function createHexagon(x_pos, y_pos) {
 			selectable: false
 		}
 	);
-	canvas.add(hexagon);
+}
+
+function createTriangle(x, y, x_pos, y_pos) {
+	this.x = x;
+	this.y = y;
+	this.x_pos = x_pos;
+	this.y_pos = y_pos;
+	this.map = [];
+	this.cds = [
+		{ x: this.x_pos+radius*-0.5, y: this.y_pos },
+		{ x: this.x_pos+radius*0.5, y: this.y_pos },
+		{ x: this.x_pos, y: this.y_pos+slant }
+	];
+	if(this.x%2 == 1) {
+		this.cds = [
+			{ x: this.x_pos, y: this.y_pos },
+			{ x: this.x_pos+radius*0.5, y: this.y_pos+slant },
+			{ x: this.x_pos+radius*-0.5, y: this.y_pos+slant }
+		];
+	}
+	this.tri = new fabric.Polygon(
+		this.cds, 
+		{
+			fill: "rgba("+(128+Math.random()*64)+","+(128+Math.random()*64)+","+(128+Math.random()*64)+","+(0.25+Math.random()*0.75)+")",
+			stroke: 'black',  
+            strokeWidth: 0.5,
+			//borderColor: 'black',
+			//cornerColor: 'black',
+			selectable: false
+		}
+	);
 }
--- main.js
+++ main.js
@@ -1,40 +1,259 @@
-var characters = [];
-var hexagons = [];
+var names = ["Enemy"];
+var characters = [[],[]];
+var planes = [];
+var characterTetra, partyHex, groupTri;
 
-const width = 24;
-const height = 21;
+characters[1][0] = new createCharacter(
+	names[0], 
+	0.5, 0.5, 
+	10, 10, 10, 11, 11, 11,
+	false, false, false, false, false, true
+);
 
-for(var i=0; i<20; i++) {
-	for(var j=0; j<20; j++) {
-		hexagons.push(new Hexagon(i, j, radius, i+", "+j));
-	}	
-}
+
 
 function setup() {
-	for(var i=0; i<width/2; i++) {
-		hexagons[i] = [];
+	for(var i=0; i<width; i+=2) {
+		planes[i] = [];
+		planes[i+1] = [];
 		for(var j=0; j<height; j++) {
-			hexagons[i][j] = new Hexagon(i, j, radius, i+", "+j);
-			hexagons[i][j].hex = createHexagon(i*radius*3, j*slant*2);
-			hexagons[i][j] = new Hexagon(i, j, radius, i+", "+j);
-			hexagons[i][j].hex = createHexagon(i*radius*3+(radius*1.5), j*slant*2-(slant));
+			if(j%2 == 0) {
+				planes[i][j] = new createTriangle(i, j, i*radius*0.5, j*slant);
+				planes[i+1][j] = new createTriangle(i+1, j, (i+1)*radius*0.5, j*slant);
+			} else {
+				planes[i][j] = new createTriangle(i, j, (i+1)*radius*0.5, j*slant);
+				planes[i+1][j] = new createTriangle(i+1, j, (i+2)*radius*0.5, j*slant);
+			}
 		}	
+	}
+	for(var i=0; i<width; i+=2) {
+		planes[groupX][groupY].map[i] = [];
+		planes[groupX][groupY].map[i+1] = [];
+		for(var j=0; j<height; j++) {
+			planes[groupX][groupY].map[i][j] = new createHexagon(i, j, i*radius*1.5, j*slant*2);
+			planes[groupX][groupY].map[i+1][j] = new createHexagon(i+1, j, (i+1)*radius*1.5, j*slant*2-(slant));
+		}	
+	}
+	for(var i=0; i<width; i+=2) {
+		planes[groupX][groupY].map[partyX][partyY].grid[i] = [];
+		planes[groupX][groupY].map[partyX][partyY].grid[i+1] = [];
+		for(var j=0; j<height; j++) {
+			planes[groupX][groupY].map[partyX][partyY].grid[i][j] = new createSquare(i, j, i*radius, j*radius, planes[groupX][groupY].map[partyX][partyY].c);
+			planes[groupX][groupY].map[partyX][partyY].grid[i+1][j] = new createSquare(i+1, j, (i+1)*radius, j*radius, planes[groupX][groupY].map[partyX][partyY].c);
+		}	
+	}
+	redraw(1);
+}
+
+
+function redraw(scale) {
+	if(scale == 0) {
+		for(var i=0; i<width; i++) {
+			for(var j=0; j<height; j++) {
+				canvas.add(planes[groupX][groupY].map[partyX][partyY].grid[i][j].tetra);
+			}
+		}
+		characterTetra = new fabric.Polygon(
+			[
+				{ x: planes[groupX][groupY].map[partyX][partyY].grid[characterX][characterY].x_pos, y: planes[groupX][groupY].map[partyX][partyY].grid[characterX][characterY].y_pos },
+				{ x: planes[groupX][groupY].map[partyX][partyY].grid[characterX][characterY].x_pos+radius, y: planes[groupX][groupY].map[partyX][partyY].grid[characterX][characterY].y_pos },
+				{ x: planes[groupX][groupY].map[partyX][partyY].grid[characterX][characterY].x_pos+radius, y: planes[groupX][groupY].map[partyX][partyY].grid[characterX][characterY].y_pos+radius },
+				{ x: planes[groupX][groupY].map[partyX][partyY].grid[characterX][characterY].x_pos, y: planes[groupX][groupY].map[partyX][partyY].grid[characterX][characterY].y_pos+radius },
+			], 
+			{
+				stroke: 'black',  
+				strokeWidth: 0.5,
+				//borderColor: 'black',
+				//cornerColor: 'black',
+				opacity: 0.66,
+				selectable: false
+			}
+		);
+		canvas.add(characterTetra);
+	} else if(scale == 1) {
+		for(var i=0; i<width; i++) {
+			for(var j=0; j<height; j++) {
+				canvas.add(planes[groupX][groupY].map[i][j].hex);
+			}
+		}
+		partyHex = new fabric.Polygon(
+			[
+				{ x: planes[groupX][groupY].map[partyX][partyY].x_pos+(radius/2), y: planes[groupX][groupY].map[partyX][partyY].y_pos },
+				{ x: planes[groupX][groupY].map[partyX][partyY].x_pos+(radius*1.25), y: planes[groupX][groupY].map[partyX][partyY].y_pos+(slant/2) },
+				{ x: planes[groupX][groupY].map[partyX][partyY].x_pos+(radius*1.25), y: planes[groupX][groupY].map[partyX][partyY].y_pos+(slant*1.5) },
+				{ x: planes[groupX][groupY].map[partyX][partyY].x_pos+(radius/2), y: planes[groupX][groupY].map[partyX][partyY].y_pos+(slant*2) },
+				{ x: planes[groupX][groupY].map[partyX][partyY].x_pos+(radius*-0.25), y: planes[groupX][groupY].map[partyX][partyY].y_pos+(slant*1.5) },
+				{ x: planes[groupX][groupY].map[partyX][partyY].x_pos+(radius*-0.25), y: planes[groupX][groupY].map[partyX][partyY].y_pos+(slant/2) }
+			], 
+			{
+				stroke: 'black',  
+				strokeWidth: 0.5,
+				//borderColor: 'black',
+				//cornerColor: 'black',
+				opacity: 0.66,
+				selectable: false
+			}
+		);
+		canvas.add(partyHex);
+	} else if(scale == 2) {
+		for(var i=0; i<width; i++) {
+			for(var j=0; j<height; j++) {
+				canvas.add(planes[groupX][groupY].tri);
+			}
+		}
+		groupTri = new fabric.Circle(
+			{
+				left: planes[groupX][groupY].x_pos-(radius*0.25), 
+				top: planes[groupX][groupY].y_pos+(slant*0.125),
+				radius: radius/4,
+				fill: "black",
+				stroke: 'black',  
+				strokeWidth: 0.25,
+				//borderColor: 'black',
+				//cornerColor: 'black',
+				selectable: false
+			}
+		);
+		canvas.add(groupTri);
 	}
 }
 
-function draw() {
-	/*
-	for(var i=0; i<20; i++) {
-		for(var j=0; j<20; j++) {
-			
-		}	
+function clear() {
+	if(scale == 0) {
+		for(var i=0; i<width; i++) {
+			for(var j=0; j<height; j++) {
+				canvas.remove(planes[groupX][groupY].map[partyX][partyY].grid[i][j].tetra);
+			}
+		}
+		canvas.remove(characterTetra);
+	} else if(scale == 1) {
+		for(var i=0; i<width; i++) {
+			for(var j=0; j<height; j++) {
+				canvas.remove(planes[groupX][groupY].map[i][j].hex);
+			}
+		}
+		canvas.remove(partyHex);
+	} else if(scale == 2) {
+		for(var i=0; i<width; i++) {
+			for(var j=0; j<height; j++) {
+				canvas.remove(planes[groupX][groupY].tri);
+			}
+		}
+		canvas.remove(groupTri);
 	}
-	*/
+	
 }
+
+function characterTetraChange(dX, dY) {
+	characterX += dX;
+	characterY += dY;
+	if(characterX < 0) {
+		characterX = 0;
+	}
+	if(characterX > width-1) {
+		characterX = width-1;
+	}
+	if(characterY < 0) {
+		characterY = 0;
+	}
+	if(characterY > height-1) {
+		characterY = height-1;
+	}
+	clear(0);
+	redraw(0);
+}
+function partyHexChange(dX, dY) {
+	partyX += dX;
+	partyY += dY;	
+	if(partyX < 0) {
+		partyX = 0;
+	}
+	if(partyX > width-1) {
+		partyX = width-1;
+	}
+	if(partyY < 0) {
+		partyY = 0;
+	}
+	if(partyY > height-1) {
+		partyY = height-1;
+	}
+	clear(1);
+	redraw(1);
+}
+function groupTriChange(dX, dY) {
+	groupX += dX;
+	groupY += dY;
+	if(groupX < 0) {
+		groupX = 0;
+	}
+	if(groupX > width-1) {
+		groupX = width-1;
+	}
+	if(groupY < 0) {
+		groupY = 0;
+	}
+	if(groupY > height-1) {
+		groupY = height-1;
+	}
+	clear(2);
+	redraw(2);
+}
+
+window.addEventListener('keydown', (e) => {
+	let dX = 0, dY = 0;
+	switch(e.code) {
+		case "KeyW":
+			dY = -1;
+			e.returnValue = false;
+			break;
+		case "KeyD":
+			dX = 1;
+			e.returnValue = false;
+			break;
+		case "KeyS":
+			dY = 1;
+			e.returnValue = false;
+			break;
+		case "KeyA":
+			dX = -1;
+			e.returnValue = false;
+			break;
+    }
+    if(scale == 0) {
+		characterTetraChange(dX, dY);
+	} else if(scale == 1) {
+		partyHexChange(dX, dY);
+	} else if(scale == 2) {
+		groupTriChange(dX, dY);
+	}
+});
+document.getElementById("local").addEventListener("click", function() {
+	clear(scale);
+	scale = 0;
+	redraw(scale);
+	document.getElementById("scale").innerHTML = "Scale: "+scales[scale];
+});
+
+document.getElementById("geographic").addEventListener("click", function() {
+	clear(scale);
+	scale = 1;
+	redraw(scale);
+	document.getElementById("scale").innerHTML = "Scale: "+scales[scale];
+});
+
+document.getElementById("planar").addEventListener("click", function() {
+	clear(scale);
+	scale = 2;
+	redraw(scale);
+	document.getElementById("scale").innerHTML = "Scale: "+scales[scale];
+});
+			
 
 setup();
 /*
 setInterval(function() {
-    draw();
+    if(running) {
+		draw();
+	}
 }, 3000);
 */
